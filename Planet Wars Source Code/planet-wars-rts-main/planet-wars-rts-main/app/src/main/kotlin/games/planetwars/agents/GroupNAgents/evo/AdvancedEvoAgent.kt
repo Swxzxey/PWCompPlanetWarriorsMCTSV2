1c04c514aec47888e7b647a421e9b9ab3ad710bd
Melih Ustun <melihuustun@gmail.com>
2026-04-11 22:22:00 +0100

    AdvancedEvoAgent improved with the following changes: AdvancedForwardModel used to receive growth rate which is factored into the score of a solution; the economy difference between the player and the opponent is now factored into the success of a solution. A 3 value sequence is used to incorporate variable nShips into the evo model. The sequenceLength is increased for greater depth in each forward model simulation Economy weight is adjusted depending on how far though the game is at any point in time.

--- Planet Wars Source Code/planet-wars-rts-main/planet-wars-rts-main/app/src/main/kotlin/games/planetwars/agents/GroupNAgents/evo/AdvancedEvoAgent.kt
+++ Planet Wars Source Code/planet-wars-rts-main/planet-wars-rts-main/app/src/main/kotlin/games/planetwars/agents/GroupNAgents/evo/AdvancedEvoAgent.kt
@@ -6,22 +6,29 @@ import games.planetwars.agents.Action
 import games.planetwars.agents.DoNothingAgent
 import games.planetwars.agents.PlanetWarsAgent
 import games.planetwars.agents.PlanetWarsPlayer
+import games.planetwars.agents.*
+import games.planetwars.agents.GroupNAgents.DefensiveReactiveAgent.DefensiveReactiveAgent10
+import games.planetwars.agents.evo.SimpleEvoAgent
+import games.planetwars.agents.evo.SimpleEvoAgent.ScoredSolution
+import games.planetwars.agents.random.BetterRandomAgent
+import games.planetwars.agents.random.CarefulRandomAgent
 import games.planetwars.core.*
 import kotlin.random.Random
 
-data class GameStateWrapper(
+data class AdvancedGameStateWrapper(
     val gameState: GameState,
     val params: GameParams,
     val player: Player,
     val opponentModel: PlanetWarsAgent = DoNothingAgent(),
 ) {
-    var forwardModel = ForwardModel(gameState, params)
+    var forwardModel = AdvancedForwardModel(gameState, params)
 
     companion object {
-        val shiftBy = 2
+        // nShips is also included in forward model being used
+        val shiftBy = 3
     }
 
-    fun getAction(gameState: GameState, from: Float, to: Float): Action {
+    fun getAction(gameState: GameState, from: Float, to: Float, numShips: Float = 0.5f): Action {
         // filter the planets that are owned by the player AND have a transporter available
         val myPlanets = gameState.planets.filter { it.owner == player && it.transporter == null }
         // filter the planets that are owned by the player AND have a transporter available
@@ -35,17 +42,19 @@ data class GameStateWrapper(
         }
         val source = myPlanets[(from * myPlanets.size).toInt()]
         val target = otherPlanets[(to * otherPlanets.size).toInt()]
-        return Action(player, source.id, target.id, source.nShips / 2)
+        return Action(player, source.id, target.id, source.nShips * numShips)
     }
 
     fun runForwardModel(seq: FloatArray): Double {
         var ix = 0;
-        forwardModel = ForwardModel(gameState.deepCopy(), params)
+        forwardModel = AdvancedForwardModel(gameState.deepCopy(), params)
         while (ix < seq.size && !forwardModel.isTerminal()) {
             val from = seq[ix]
             val to = seq[ix + 1]
-            val myAction = getAction(gameState, from, to)
-            val opponentAction = opponentModel.getAction(gameState)
+            val nShips = seq[ix + 2]
+            // The gameState is taken from the forward model to allow for dynamic reactive gameplay
+            val myAction = getAction(forwardModel.state, from, to, nShips)
+            val opponentAction = opponentModel.getAction(forwardModel.state)
             val actions = mapOf(player to myAction, player.opponent() to opponentAction)
             forwardModel.step(actions)
             ix += shiftBy
@@ -57,21 +66,36 @@ data class GameStateWrapper(
         // allow standalone use of this as well
         return forwardModel.getShips(player) - forwardModel.getShips(player.opponent())
     }
+
+    fun growthRateDifference(): Double {
+        return forwardModel.getGrowthRate(player) - forwardModel.getGrowthRate(player.opponent())
+    }
 }
 
 data class AdvancedEvoAgent(
     var flipAtLeastOneValue: Boolean = true,
     var probMutation: Double = 0.5,
-    var sequenceLength: Int = 200,
+    //var sequenceLength: Int = 200,
+    var sequenceLength: Int = 600,
     var nEvals: Int = 20,
     var useShiftBuffer: Boolean = true,
     var epsilon: Double = 1e-6,
     var timeLimitMillis: Long = 20,
     var opponentModel: PlanetWarsAgent = DoNothingAgent(),
+    var economyWeight: Double = 75.0,
+    var ecoWeight: Double = economyWeight,
+    var secondPart: Double = 0.35,
 
     ) : PlanetWarsPlayer() {
     override fun getAgentType(): String {
         return "AdvancedEvoAgent-$sequenceLength-$nEvals-$probMutation-$useShiftBuffer"
+    }
+
+    // neutral bug fixed by preparing the enemy agent
+    override fun prepareToPlayAs(player: Player, params: GameParams, opponent: String?): String {
+        super.prepareToPlayAs(player, params, opponent)
+        opponentModel.prepareToPlayAs(player.opponent(), params, opponent)
+        return getAgentType()
     }
 
     internal var random = Random
@@ -86,26 +110,39 @@ data class AdvancedEvoAgent(
 
     override fun getAction(gameState: GameState): Action {
 
+        // start timer
+        val startTime = System.currentTimeMillis()
+
+        var inSecondPart = false
+
+        if (inSecondPart || gameState.gameTick + sequenceLength * 0.5 > params.maxTicks * secondPart) {
+            ecoWeight = economyWeight * 0.3
+            inSecondPart = true
+        }
+
         if (bestSolution == null || !useShiftBuffer) {
             val solution = randomPoint()
-            bestSolution = ScoredSolution(evalSeq(gameState, solution), solution)
+            val scores = evalSeq(gameState, solution)
+            bestSolution = ScoredSolution(scores.score + ecoWeight * scores.growthRate, solution)
         } else {
             val nextSeq = shiftLeftAndRandomAppend(bestSolution!!.solution, GameStateWrapper.shiftBy)
-            bestSolution = ScoredSolution(evalSeq(gameState, nextSeq), nextSeq)
+            val scores = evalSeq(gameState, nextSeq)
+            bestSolution = ScoredSolution(scores.score + ecoWeight * scores.growthRate, nextSeq)
         }
 
         // Time-bounded evolution: mutate and evaluate until the time budget runs out
-        val startTime = System.currentTimeMillis()
         while (System.currentTimeMillis() - startTime < timeLimitMillis) {
+            //println(System.currentTimeMillis() - startTime)
             val mut = mutate(bestSolution!!.solution, probMutation)
-            val mutScore = evalSeq(gameState, mut)
+            val scores = evalSeq(gameState, mut)
+            val mutScore = scores.score + ecoWeight * scores.growthRate
             if (mutScore >= bestSolution!!.score) {
                 bestSolution = ScoredSolution(mutScore, mut)
             }
         }
 
-        val wrapper = GameStateWrapper(gameState, params, player)
-        val action = wrapper.getAction(gameState, bestSolution!!.solution[0], bestSolution!!.solution[1])
+        val wrapper = AdvancedGameStateWrapper(gameState, params, player)
+        val action = wrapper.getAction(gameState, bestSolution!!.solution[0], bestSolution!!.solution[1], bestSolution!!.solution[2])
         return action
     }
 
@@ -147,17 +184,24 @@ data class AdvancedEvoAgent(
             p[i] = v[i + shiftBy]
         }
         // TODO: this is a bit of a hack, but it should work when shiftBy is 2, which it is for now
-        p[p.size - 1] = random.nextFloat()
-        p[p.size - 2] = random.nextFloat()
+        // shift by n
+        for (i in 1 until shiftBy + 1) {
+            p[p.size - i] = random.nextFloat()
+        }
+        //p[p.size - 1] = random.nextFloat()
+        //p[p.size - 2] = random.nextFloat()
         return p
     }
 
-    private fun evalSeq(state: GameState, seq: FloatArray): Double {
-        val wrapper = GameStateWrapper(state.deepCopy(), params, player, opponentModel)
-        wrapper.runForwardModel(seq)
-        return wrapper.scoreDifference()
-    }
+    data class EvalResults(val score: Double, val growthRate: Double)
 
+    private fun evalSeq(state: GameState, seq: FloatArray): EvalResults {
+        val wrapper = AdvancedGameStateWrapper(state.deepCopy(), params, player, opponentModel)
+        wrapper.runForwardModel(seq)
+        val score = wrapper.scoreDifference()
+        val growthRate = wrapper.growthRateDifference()
+        return EvalResults(score, growthRate)
+    }
 }
 
 fun main() {
