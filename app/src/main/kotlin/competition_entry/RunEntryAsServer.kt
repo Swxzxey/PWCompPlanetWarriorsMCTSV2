88731d301f6ef2f6f071ac2bc7aab2e3f6b2d391
Nikolay <ec24311@qmul.ac.uk>
2026-05-19 06:06:55 +0100

    Adjusted RunEntryAsServer.kt for MCTS Agent entry.

--- app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
+++ app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
@@ -1,9 +1,9 @@
 package competition_entry
 
-import games.planetwars.agents.GroupNAgents.evo.PlanetWarriorV2
+import games.planetwars.agents.GroupNAgents.MCTS.MCTSAgentV2
 import json_rmi.GameAgentServer
 
 fun main() {
-    val server = GameAgentServer(port = 8080, agentClass = PlanetWarriorV2::class)
+    val server = GameAgentServer(port = 8080, agentClass = MCTSAgentV2::class)
     server.start(wait = true)
 }
